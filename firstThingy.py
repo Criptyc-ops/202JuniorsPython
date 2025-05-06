d69cda419a2bff040f739c4e26bb9ab7ad650d5b
Criptyc-ops <cte-jonathan.robinson@sllboces.org>
2025-05-06 17:41:16 +0000

    wowzers

---
+++ firstThingy.py
@@ -0,0 +1,13 @@
+firstname = input("Enter your first name: ")
+
+print("Hello " + firstname + "! " "whats your favorite afternoon activity?")
+activity = input("Enter your favorite activity: ")
+
+print("wow " + firstname + "!" + " I hope you enjoy " + activity + "!")
+lunch = input("What is your favorite food to have for lunch? ")
+
+print("I hope you enjoy " + lunch + " for lunch!")
+
+print("have a great day " + firstname + "!")
+
+
---
+++ hello.py
@@ -0,0 +1,5 @@
+# create a variable
+name = input('Enter your name: ')
+# print the variable
+
+print('hello World ' + name + '!')
